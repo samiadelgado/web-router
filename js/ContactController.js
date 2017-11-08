981b6eb14e8510b0f289d161f823513c0dbcd27c
Sami A.Delgado <samiadelgado@gmail.com>
2017-11-08 12:47:28 +0100

    github.io

--- js/ContactController.js
+++ js/ContactController.js
@@ -10,7 +10,6 @@ function ContactController(){
 	    var map = new google.maps.Map(document.getElementById("map"),
 	            myOptions);
 	}
-
 	google.maps.event.addDomListener(window, "load", initialize);
 }
 
